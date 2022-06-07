850ed99d81e4aed093d35a26522899802dbb7dfe
StefanoScolari <stefano.scolari@studio.unibo.it>
2022-06-07 18:17:14 +0200

    fix db

--- app/src/main/java/com/app/mypresence/model/database/AppDatabase.java
+++ app/src/main/java/com/app/mypresence/model/database/AppDatabase.java
@@ -36,7 +36,7 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
-@Database(entities = {User.class, DateInfo.class}, version = 21)
+@Database(entities = {User.class, DateInfo.class}, version = 22)
 @TypeConverters({Converters.class})
 public abstract class AppDatabase extends RoomDatabase {
 
